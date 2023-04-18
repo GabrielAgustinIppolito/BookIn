7a0003eb01dddf583176eea691ef1e4b2a180fff
PCTAlessio <picatti.alessio@gmail.com>
2023-04-18 10:28:03 +0200

    aggiunge service e controller

--- src/main/java/com/hufflepuff/generation/italy/BookIn/restController/BookController.java
+++ src/main/java/com/hufflepuff/generation/italy/BookIn/restController/BookController.java
@@ -1,13 +1,21 @@
 package com.hufflepuff.generation.italy.BookIn.restController;
 
 import com.hufflepuff.generation.italy.BookIn.model.data.abstractions.AbstractBookRepository;
+import com.hufflepuff.generation.italy.BookIn.model.services.abstractions.AbstractBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
+
+    private AbstractBookService service;
+
     @Autowired
-    private AbstractBookRepository repo;
+    public BookController(AbstractBookService service){
+        this.service = service;
+    }
+
+
 
 
 }
